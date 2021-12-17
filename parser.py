779538bf1f159959cbac0b5bf64d6b4cf38ed8bb
SrkbskiDobrovolci <gfnmjg@gmail.com>
2021-12-17 23:33:30 +0300

    Add normal request to dns site

--- parser.py
+++ parser.py
@@ -28,8 +28,6 @@ class AbstractParser():
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
         wait = WebDriverWait(self.driver, 20)
-        self.url = url
-        self.soup = None
 
     def send_data(self):
         pass
@@ -45,27 +43,44 @@ class AbstractParser():
 class ParserDNS(AbstractParser):
     def __init__(self, city, product_name, url):
         AbstractParser.__init__(self, city, product_name, url)
-        self.driver.get(url)
-        self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        temp_str = self.product_name
 
+        self.url = url + "search/?q=" + self.product_name.replace(' ', '+')
+
+        self.product_name = temp_str
+
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        WebDriverWait(self.driver, 20)
+        self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        time.sleep(3)
     def set_city(self):
-        city_soup = self.soup.find("a" , {"class" : "w-choose-city-widget pseudo-link pull-right"})
+        city_soup = self.soup.find("a", {"class": "w-choose-city-widget pseudo-link pull-right"})
         selenium_path_element = self.to_xpath(city_soup)
         ActionChains(self.driver).move_to_element(selenium_path_element).click().perform()
 
         time.sleep(2)
         self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-        city_soup = self.soup.find("input" , {"data-role" : "search-city"})
+        city_soup = self.soup.find("input", {"data-role": "search-city"})
         selenium_path_element = self.to_xpath(city_soup)
-        ActionChains(self.driver).move_to_element(selenium_path_element).click().send_keys(self.city).send_keys(Keys.ENTER).perform()
+        ActionChains(self.driver).move_to_element(selenium_path_element).click().send_keys(self.city).send_keys(
+            Keys.ENTER).perform()
 
-    def send_data(self):
-        input_data = self.soup.find("a" , {"class" : "w-choose-city-widget pseudo-link pull-right"})
-        selenium_path_element = self.to_xpath(input_data)
-        ActionChains(self.driver).move_to_element(selenium_path_element).click().send_keys(self.product_name).perform()
+    def read_data(self):
+        self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
+        filter_soup = self.soup.find("span", {"class": "top-filter__selected"})
+        selenium_filter_path = self.to_xpath(filter_soup)
+        ActionChains(self.driver).move_to_element(selenium_filter_path).click().perform()
+
+        '''
+        price_soup = self.soup.find("div", {"class": "product-buy__price"})
+        price_text = price_soup.text
+        print(price_text)
+        '''
 
 
 a = ParserDNS("Москва", "geforce gtx 1650", "https://www.dns-shop.ru/")
 a.set_city()
+a.read_data()
