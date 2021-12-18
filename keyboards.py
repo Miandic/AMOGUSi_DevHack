d100fb99a013b0977f2729b765c720be93e14a6d
Gusarich <danielsedovzzz@gmail.com>
2021-12-18 14:18:14 +0300

    bot now corrects city input from user

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,9 @@
 from aiogram.types import *
 
-
-max_price_question_yes = InlineKeyboardButton('Да', callback_data='max_price_question_yes')
-max_price_question_no = InlineKeyboardButton('Нет', callback_data='max_price_question_no')
 max_price_question_kb = InlineKeyboardMarkup()
-max_price_question_kb.add(max_price_question_yes)
-max_price_question_kb.add(max_price_question_no)
+max_price_question_kb.add(InlineKeyboardButton('Да', callback_data='max_price_question_yes'))
+max_price_question_kb.add(InlineKeyboardButton('Нет', callback_data='max_price_question_no'))
+
+guessed_city_kb = InlineKeyboardMarkup()
+guessed_city_kb.add(InlineKeyboardButton('Да', callback_data='guessed_city_yes'))
+guessed_city_kb.add(InlineKeyboardButton('Нет', callback_data='guessed_city_no'))
